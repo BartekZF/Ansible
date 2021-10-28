0f3e42e921d5463d962321d4d85dcd1aecb38b12
Depta Bartosz CZS FIIA12 <Z0051309@ad.trw.com>
2021-10-28 11:35:19 +0200

    scrpit version 2

--- cheking_mounts/checking_mount.sh
+++ cheking_mounts/checking_mount.sh
@@ -1,24 +1,34 @@
 #!/bin/bash
 
-touch /home/cloud_user/checking_mounts.log
+log_path= "<Path to log>"
+touch ${log_path}/checking_mounts.log
 
-MOUNTPOINT1="/some/mount-point"
+MOUNTPOINT1="<Path to mount>"
+#MOUNTPOINT2="<Path to mount>"
 
-log = /home/cloud_user/checking_mounts.log
+SOURCEPOINT1="Source path"
+#SOURCEPOINT2=""
 
+SECRETS="<path_to_credentials>" 
+
+LOG = ${log_path}/checking_mounts.log
+#### MOUNTPOINT 1 ###
+
+#Checking if file system is already mounted, if not it will use SOURCEPOINT1 and it will mount it in MOUNTPOINT1. Log will be available in LOG
 if cat /proc/mounts | grep ${MOUNTPOINT1} > /dev/null; then
-        echo "${MOUNTPOINT1} already mounted." >> log
-        exit 0
-fi
-
-# If above did not exit
-mount ${MOUNTPOINT1}
-
-if [ $? -eq 0 ]; then
-        echo "${MOUNTPOINT1} has been mounted." >> log
+        echo $(date) ": ${MOUNTPOINT1} already mounted." >> ${LOG}
 else
-        # Mounting failed, send email
-        echo "Mounting of ${MOUNTPOINT1} failed."  >> log 
+        mount -t cifs -o credentials=${SECRETS} ${SOURCEPOINT1} ${MOUNTPOINT1}
+        echo $(date) ": ${MOUNTPOINT1} has been mounted." >> ${LOG}
 fi
+
+#### MOUNTPOINT 2 ###
+#Checking if file system is already mounted, if not it will use SOURCEPOINT2 and it will mount it in MOUNTPOINT2. Log will be available in LOG
+# if cat /proc/mounts | grep ${MOUNTPOINT2} > /dev/null; then
+#         echo $(date) ": ${MOUNTPOINT2} already mounted." >> ${LOG}
+# else
+#         mount -t cifs -o credentials=${SECRETS} ${SOURCEPOINT2} ${MOUNTPOINT2}
+#         echo $(date) ": ${MOUNTPOINT2} has been mounted." >> ${LOG}
+# fi
 
 exit
